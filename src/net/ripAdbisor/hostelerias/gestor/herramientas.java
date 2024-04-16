4970d54b99d6d672c199258baf9fa74ae81488b7
Giovanni <usr3855@salesianos-lacuesta.net>
2024-04-16 12:52:01 +0100

    menu y herramientas

--- src/net/ripAdbisor/hostelerias/gestor/herramientas.java
+++ src/net/ripAdbisor/hostelerias/gestor/herramientas.java
@@ -1,5 +1,36 @@
 package net.ripAdbisor.hostelerias.gestor;
 
+import java.util.ArrayList;
+
 public class herramientas {
-    
+
+    ArrayList<String> listaRestaurante = new ArrayList<>();
+
+    private String nombre;
+    private String direccion;
+    private int aforo;
+    private String tipoRestaurante;
+
+    public herramientas(String nombre, String direccion, int aforo, String tipoRestaurante) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.aforo = aforo;
+        this.tipoRestaurante = tipoRestaurante;
+    }
+
+    public void añadirRestaurante() {
+        
+    }
+
+    public void editarRestaurante() {
+
+    }
+
+    public void mostrarRestaurante() {
+
+    }
+
+    public void eliminarRestaurante() {
+
+    }
 }
--- src/net/ripAdbisor/hostelerias/menu/menu.java
+++ src/net/ripAdbisor/hostelerias/menu/menu.java
@@ -2,4 +2,16 @@ package net.ripAdbisor.hostelerias.menu;
 
 public class menu {
     
+    public void mostrarMenu() {
+        System.out.println("""
+                *****************************
+                    SELECCIONA UNA OPCION
+                    1. Añadir restaurante
+                    2. Editar restaurante
+                    3. Mostrar restaurante 
+                    4. Eliminar empleado
+                    Q. Salir del programa
+                *****************************
+                """);
+    }
 }
